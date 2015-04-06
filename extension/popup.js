43273a9a941549e3b3748618495a17d9a10df86f
Erich Lotto <erichlotto@gmail.com>
2015-04-06 13:12:36 -0300

    Added supoport for Vagalume API, removed all requests to my personal server, songs are cached individually

--- extension/popup.js
+++ extension/popup.js
@@ -2,47 +2,68 @@
 chrome.tabs.getSelected(null, function(tab) {
 	chrome.tabs.sendMessage(tab.id, {query:"getInfo" },
 		function(response) {
-
 			// We need to check if the user is actually playing a song
-			if(!response.isPlaying){
+			if(!response.isPlaying)
 				$("#status").html("Play a song first ;)");
-				return;
-			}
-
-			// Check if we already hare this song cached
-			if(response.cachedLyric.length>0){
-					// It's cached, fill the popup
-					fillPopUpWithLyrics(response.currentArtist, response.currentSong, response.cachedLyric/*+"<br/><small style=\"float:right;color:red;\">cached</small>"*/);
-			} else {
-
-				// We dont have it cached, make the request to the external server
-				$.get( "http://erichlotto.com/api/lyrics/?artist="+response.currentArtist+"&song="+response.currentSong, function( data ) {
-
-					// Fill the popup
-					fillPopUpWithLyrics(response.currentArtist, response.currentSong, data);
-
-					// Pass back the lyrics to content_script, so if the user opens the popup again we don't need to fetch the song twice
-					chrome.tabs.sendMessage(tab.id, {query:"storeLyric", artist:response.currentArtist, song:response.currentSong, lyrics: data});
-   				}).fail(function(){
-					// Something went wrong with the request. Alert the user
-					$("#status").html("There was an error processing your request.");
-				});
-			}
+			else
+				fetchLetra(response.currentArtist, response.currentSong);
 		}
 	);
 });
 
-function fillPopUpWithLyrics(artist, song, lyrics){
-	var top = "<h2>"+song + "</h2><br/><i>by <h4>" +artist+"</h4></i><br/><br/>";
-	$("#status").html(top+lyrics);
-	$("#top_bar").css("display","inherit")
-	$("#new_window").click(function(){openPopup(artist, song, lyrics);});
-}
 
 function openPopup(artist, song, lyrics){
+
 	chrome.runtime.getBackgroundPage(function(bgWindow) {
 		bgWindow.storeBackgroundTempData(artist, song, lyrics);
 		chrome.windows.create({'url': 'popup_window.html', 'type': 'detached_panel', 'width': $(window).width()+20, 'height': $(window).height()-20, 'focused':true }, function(window) {
 		});
+	});
+}
+
+
+
+function showLetra (data,art,mus,arrayid) {
+	if (! arrayid) arrayid = 0;
+
+	if (data.type == 'exact' || data.type == 'aprox') {
+		// Print lyrics text
+		var top = "<h2>"+mus + "</h2><br/><i>by <h4>" +art+"</h4></i><br/><br/>";
+		$("#status").html(top+data.mus[arrayid].text);
+		$("#top_bar").css("display","inherit")
+		$("#new_window").click(function(){openPopup(art, mus, data.mus[arrayid].text);});
+	} else if (data.type == 'song_notfound') {
+		// Song not found, but artist was found
+		// You can list all songs from Vagalume here
+		$("#status").text("could not find song \""+mus+"\" by "+art);
+	} else {
+		// Artist not found
+		$("#status").text("could not find artist "+art);
+	}
+}
+
+function fetchLetra (art,mus) {
+	var data = jQuery.data(document,art + mus); // cache read
+	if (data) {
+		showLetra(data, art, mus);
+		return true;
+	}
+
+	var url = "http://api.vagalume.com.br/search.php"
+		+"?art="+encodeURIComponent(art)
+		+"&mus="+encodeURIComponent(mus);
+
+	// Check if browser supports CORS - http://www.w3.org/TR/cors/
+	if (!jQuery.support.cors) {
+		url += "&callback=?";
+	}
+
+	jQuery.getJSON(url,function(data) {
+		// What we do with the data
+		jQuery.data(document,art + mus,data); // cache write
+		showLetra(data, art, mus);
+	}).fail(function(){
+		// Something went wrong with the request. Alert the user
+		$("#status").text("There was an error processing your request.");
 	});
 }
