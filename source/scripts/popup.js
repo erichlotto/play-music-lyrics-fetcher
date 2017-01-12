06ba1aed714c4092bf260c6fa1026cc3964abd23
AndersonMamede <avatar04@hotmail.com>
2017-01-12 10:29:50 -0200

    Solves #1 by using window.open with the same WindowID instead of chrome.windows API

--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -47,13 +47,19 @@ chrome.storage.sync.get('display_new_window', function(obj) {
 
 
 function openPopup(artist, song, lyrics){
-
 	chrome.runtime.getBackgroundPage(function(bgWindow) {
 		bgWindow.storeBackgroundTempData(artist, song, lyrics);
-		chrome.windows.create({'url': '../pages/popup_window.html', 'type': 'detached_panel', 'width': $(window).width()+30, 'height': $(window).height()-20, 'focused':true });
+		
+		// if there is a window with the same id, it'll be refreshed
+		// instead of creating another window
+		var lyricsPopup = window.open(
+			'../pages/popup_window.html',
+			'play-music-lyrics-fetcher-window'
+		);
+		lyricsPopup.focus();
+		
 		window.close();
 	});
-
 }
 
 function restart(){
