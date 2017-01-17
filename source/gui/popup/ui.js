a348a76bf5d879172a78dd6a1545904d968ba22c
Erich Lotto <erichlotto@gmail.com>
2017-01-17 16:36:08 -0200

    Cleaned up popup for initial release. SCROLL ISSUE IS PRESENT

--- source/gui/popup/ui.js
+++ source/gui/popup/ui.js
@@ -1,83 +1,62 @@
 jQuery.getJSON("/manifest.json",function(data) {
 	document.title = data.name;
-/*	chrome.storage.sync.get('last_version', function(obj) {
-		var lastVersion = obj.last_version;
-		if(!lastVersion || lastVersion != data.version){
-				displayReleaseNotes(data.version);
-		} else {
-			chrome.storage.sync.get('overlay', function(obj) {
-				if(obj.overlay)toggleOverlay();
-				else restart();
-			});
-		}
-		chrome.storage.sync.set({ 'last_version': data.version });
-	});*/
 });
 
 var currentLyrics;
 var songTimingDelay = 0;
-var autoScroll = true;
+var autoScroll = false;
 
 function onLyricsLoadStart(){
   $("#status").text("Lyrics Load Started");
   $("#status").show();
-  $("#fix_song_info").hide();
   $("#info").hide();
-  $("#top_bar").hide();
 }
 
 function onLyricsLoadFinished(lyrics){
 	currentLyrics = lyrics;
 
-  $("#status").hide();
-  $("#fix_song_info").hide();
-  $("#info").show();
-  $("#top_bar").show();
-
+	$("#status").hide();
+	$("#info").show();
 	$("#artist").text(lyrics.artist);
-  $("#track").text(lyrics.track);
+	$("#track").text(lyrics.track);
 	if(lyrics.timmed.length>0){
-		console.log(lyrics.timmed)
+        $("#lyrics").empty();
 		for(i in lyrics.timmed){
 			$("#lyrics").append("<p class=\"lyrics_line\">"+lyrics.timmed[i].text.trim()+"</p>");
-			$("#lyrics").css("display", "inherit");
+			$("#lyrics").css("display", "block");
 		}
 	} else {
 		$("#lyrics").text(lyrics.static);
 		$("#lyrics").css("white-space", "pre");
 	}
-	$("#top_bar_new_window").off("click");
-	$("#top_bar_new_window").click(function(){openPopup();});
-
 }
 
-function openPopup(){
-	chrome.windows.create({'url': './gui/popup/popup.html', 'type': 'detached_panel', 'width': $(window).width()+30, 'height': $(window).height()-20, 'focused':true });
-	window.close();
-}
 
 function onLyricsLoadError(error){
   $("#status").text(error);
   $("#status").show();
   $("#info").hide();
-	$("#top_bar").hide();
-	$("#fix_song_info").hide();
 }
 
 function onPositionChanged(position){
-	if(currentLyrics.timmed.length < 1)return;
-	console.log(currentLyrics)
+	if(!currentLyrics || currentLyrics.timmed.length < 1)return;
 	for(var i=currentLyrics.timmed.length-1; i>=0; i--){
 		if((currentLyrics.timmed[i].enter < position.position-songTimingDelay) || i == 0){
 			$( ".lyrics_line" ).removeClass( "current" );
 			$( ".lyrics_line:eq("+i+")" ).addClass( "current" );
-			if(autoScroll)
-			$('html, body').animate({
-				scrollTop: $(".current").offset().top-140
-			}, 100);
+            if(autoScroll)
+                smoothScrool();
 			break;
 		}
 
 	}
-  console.log(position);
+}
+
+
+$(document).scroll(function(){console.log('scroll')})
+
+function smoothScrool(){
+    $('html, body').animate({
+        scrollTop: $(".current").offset().top-140
+    }, 100);
 }
