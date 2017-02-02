7bf99f9bc07788a76ff2cbf35cd640edb3d2ef92
Erich Lotto <erichlotto@gmail.com>
2017-02-02 17:58:07 -0200

    Removed comments in background script

--- source/services/background.js
+++ source/services/background.js
@@ -4,12 +4,10 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     switch(request.query){
         case "SHOW_PAGE_ACTION":
-            // chrome.tabs.executeScript(sender.tab.id, {code: request.code});
             chrome.tabs.executeScript(sender.tab.id, {file: request.path});
             chrome.tabs.executeScript(sender.tab.id, {file: "./services/dependencies/jquery.min.js"});
             chrome.tabs.executeScript(sender.tab.id, {file: "./services/utils.js"});
             chrome.tabs.executeScript(sender.tab.id, {file: "./services/dom_listener.js"});
-//            chrome.tabs.executeScript(sender.tab.id, {file: "./lyrics_providers/vagalume.js"});
             chrome.tabs.executeScript(sender.tab.id, {file: "./services/lyrics_provider_chooser.js"});
             chrome.tabs.executeScript(sender.tab.id, {file: "./services/main.js"});
             chrome.pageAction.show(sender.tab.id);
