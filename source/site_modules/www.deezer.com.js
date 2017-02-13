3ddb3140580955b1dd748282bb21d3d88e706c56
Erich Lotto <erichlotto@gmail.com>
2017-02-13 17:26:57 -0200

    Added support for deezer

---
+++ source/site_modules/www.deezer.com.js
@@ -0,0 +1,29 @@
+// OLD LAYOUT
+
+function getDOMArtist(){
+    return $(".player-track-artist:eq(0) .player-track-link:eq(0)").text();
+}
+
+function getDOMTrack(){
+    return $(".player-track-title:eq(0)").text();
+}
+
+function getDOMAlbum(){
+    return 'Unknown';
+}
+
+function isDOMTrackAvailable(){
+  return (getDOMArtist().length>0 && getDOMTrack().length>0)?true:false;
+}
+
+function getDOMTrackPosition(){
+    return hmsToSecondsOnly($(".progress-time:eq(0)").text(), ':');
+}
+
+function getDOMTrackDuration(){
+    return hmsToSecondsOnly($(".progress-length:eq(0)").text(), ':');
+}
+
+function getDOMTimeElapsedElement(){
+   return $(".progress-time:eq(0)")
+}
