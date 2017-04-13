0eb45576f6357021fd01c8be43070f2ef2db5e9e
Erich Lotto <erichlotto@gmail.com>
2017-04-13 13:54:24 -0300

    Updated changelog

--- source/gui/popup/changelog.js
+++ source/gui/popup/changelog.js
@@ -12,7 +12,9 @@ jQuery.getJSON("/manifest.json",function(data) {
 
 function releaseNotes(){
     return '<p id="status"><h3 style="width: 500px">Changelog</h3>' +
-		'<ul>v 1.0.5' +
+		'<ul>v 1.0.6' +
+            '<li>Added support for SoundCloud</li>' +
+		'</ul><ul>v 1.0.5' +
             '<li>Fix for Spotify&#39;s new layout</li>' +
         '</ul><!--<label>Note: A lot changed under the hood here, might take some time until all minor features (such as light and dark themes) gets implementes again. Please be patient.</label>--></p>';
 }
