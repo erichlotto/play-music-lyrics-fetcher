fe4938e8bffaaa2cd1c630655d7342f7b749b05d
Erich Lotto <erichlotto@gmail.com>
2017-06-23 18:34:27 -0300

    Fix for Pandora's new layout

--- source/site_modules/pandora.js
+++ source/site_modules/pandora.js
@@ -1,13 +1,13 @@
 function getDOMArtist(){
-    return $(".playerBarArtist:eq(0)").text();
+    return $("a[data-qa='mini_track_artist_name']:eq(0)").text();
 }
 
 function getDOMTrack(){
-    return $(".playerBarSong:eq(0)").text();
+    return $("a[data-qa='mini_track_title']:eq(0)").text();
 }
 
 function getDOMAlbum(){
-    return $(".playerBarAlbum:eq(0)").text();
+    return 'Unknown';
 }
 
 function isDOMTrackAvailable(){
@@ -15,14 +15,13 @@ function isDOMTrackAvailable(){
 }
 
 function getDOMTrackPosition(){
-    return hmsToSecondsOnly($(".elapsedTime:eq(0)").text(), ':');
+    return hmsToSecondsOnly(getDOMTimeElapsedElement().text(), ':');
 }
 
 function getDOMTrackDuration(){
-    var trackRemaining = hmsToSecondsOnly($(".remainingTime:eq(0)").text(), ':');
-    return getDOMTrackPosition() + trackRemaining;
+    return hmsToSecondsOnly($("span[data-qa='remaining_time']:eq(0)").text(), ':');
 }
 
 function getDOMTimeElapsedElement(){
-   return $(".elapsedTime:eq(0)");
+    return $("span[data-qa='elapsed_time']:eq(0)");
 }
