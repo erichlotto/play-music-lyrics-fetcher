f36ccadb99d73c1be69581f91b6829860edcac1b
Erich Lotto <erichlotto@gmail.com>
2018-07-24 17:23:02 -0300

    Fix for autoscroll on spotify

--- source/gui/popup/changelog.js
+++ source/gui/popup/changelog.js
@@ -12,19 +12,17 @@ jQuery.getJSON("/manifest.json",function(data) {
 
 function releaseNotes(){
     return '<p id="status"><h3 style="width: 500px">Changelog</h3>' +
-		'<ul>v 1.0.12' +
+		'<ul>v 1.0.13' +
+            "<li>Fixed timed lyrics not working properly on Spotify</li>" +
+		'</ul><ul>v 1.0.12' +
             "<li>Genius integration fixed</li>" +
             "<li>Fixed timed lyrics not showing for non-US songs</li>" +
             "<li>Added option to disable window resizing in detached mode</li>" +
 		'</ul><ul>v 1.0.8' +
             "<li>Fix for YouTube's new layout</li>" +
-		'</ul><ul>v 1.0.7' +
-            '<li>Fixed Pandora integration</li>' +
-            '<li>Added option to disable timed lyrics in (check the options page)</li>' +
-            '<li>Fixed DarkLyrics integration</li>' +
-		// '</ul><ul>v 1.0.6' +
-         //    '<li>Added support for SoundCloud</li>' +
-		// '</ul><ul>v 1.0.5' +
-         //    '<li>Fix for Spotify&#39;s new layout</li>' +
-        '</ul><!--<label>Note: A lot changed under the hood here, might take some time until all minor features (such as light and dark themes) gets implementes again. Please be patient.</label>--></p>';
+		// '</ul><ul>v 1.0.7' +
+         //    '<li>Fixed Pandora integration</li>' +
+         //    '<li>Added option to disable timed lyrics in (check the options page)</li>' +
+         //    '<li>Fixed DarkLyrics integration</li>' +
+        '</ul></p>';
 }
--- source/site_modules/spotify.open.js
+++ source/site_modules/spotify.open.js
@@ -1,3 +1,8 @@
+/*
+ * Note: since release 1.0.13 we no longer use ADD_REQUEST_LISTENER to listen
+ * for the track position on Spotify. Legacy code is commented for future
+ * reference.
+ */
 function getDOMArtist(){
     return $('.track-info__artists').text().trim().split(",")[0];
 }
@@ -15,29 +20,33 @@ function isDOMTrackAvailable(){
 }
 
 function getDOMTrackPosition(){
-    var $elm = $(".now-playing-bar div:not(.extra-controls) .progress-bar__fg");
-    var percentage = $elm.width() / $elm.parent().width();
-    return trackDuration * percentage;
+    return hmsToSecondsOnly(getDOMTimeElapsedElement().text().trim(), ":");
+    // var $elm = $(".now-playing-bar div:not(.extra-controls) .progress-bar__fg");
+    // var percentage = $elm.width() / $elm.parent().width();
+    // return trackDuration * percentage;
 }
 
 function getDOMTrackDuration(){
-    return trackDuration;
+    return hmsToSecondsOnly($('.playback-bar__progress-time:eq(1)').text().trim(), ":");
+    // return trackDuration;
 }
 
 function getDOMTimeElapsedElement(){
-    return undefined;
+    return $('.playback-bar__progress-time:eq(0)');
+    // return undefined;
 }
 
-var callback = "REQUEST_RECEIVED";
-var trackDuration = -1;
-
-chrome.runtime.sendMessage({query: 'ADD_REQUEST_LISTENER', url: "https://spclient.wg.spotify.com/track-playback/*", response:callback });
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-switch(request.query) {
-    case callback:
-        if(request.body.sub_state != undefined && request.body.sub_state.duration != undefined){
-            trackDuration = request.body.sub_state.duration / 1000;
-        }
-        break;
-}});
+// var callback = "REQUEST_RECEIVED";
+// var trackDuration = -1;
+//
+// chrome.runtime.sendMessage({query: 'ADD_REQUEST_LISTENER', url: "https://spclient.wg.spotify.com/track-playback/*", response:callback });
+//
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+// switch(request.query) {
+//     case callback:
+//         if(request.body.sub_state != undefined && request.body.sub_state.duration != undefined){
+//             trackDuration = request.body.sub_state.duration / 1000;
+//             console.log('hey');
+//         }
+//         break;
+// }});
